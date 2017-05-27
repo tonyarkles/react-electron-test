b3fc3d0c8613b395494a5776672662611adbe869
Tony Arkles <t.arkles@gmail.com>
2017-05-27 15:25:22 -0600

    Using a native control. Neat!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const electron = window.require('electron');
+const {dialog} = electron.remote;
+
+const openFile = () => {
+  dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']});
+}
 
 class App extends Component {
   render() {
@@ -10,6 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React and Electron and Foreman and Other Stuff</h2>
         </div>
+        <button onClick={openFile}>Open</button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
